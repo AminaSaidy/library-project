5e7704d0dc516f3c765c8fa7a1e8e778b8c5a0ee
Your Name <your_email@whatever.com>
2025-01-06 16:03:46 +0100

    added class User

---
+++ src/main/java/uz/aminasaidakhmedova/library_project/model/User.java
@@ -0,0 +1,30 @@
+package uz.aminasaidakhmedova.library_project.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
