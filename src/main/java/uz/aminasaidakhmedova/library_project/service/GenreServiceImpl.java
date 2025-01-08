1413fc1b284e3439a04f17c30311b48e8dc045fa
Your Name <your_email@whatever.com>
2025-01-08 18:40:57 +0100

    added logging to all methods in genre service implementation

--- src/main/java/uz/aminasaidakhmedova/library_project/service/GenreServiceImpl.java
+++ src/main/java/uz/aminasaidakhmedova/library_project/service/GenreServiceImpl.java
@@ -1,6 +1,7 @@
 package uz.aminasaidakhmedova.library_project.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uz.aminasaidakhmedova.library_project.dto.AuthorDto;
 import uz.aminasaidakhmedova.library_project.dto.BookDto;
@@ -9,18 +10,25 @@ import uz.aminasaidakhmedova.library_project.model.Genre;
 import uz.aminasaidakhmedova.library_project.repository.GenreRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GenreServiceImpl implements GenreService {
 
     private final GenreRepository genreRepository;
 
     @Override
     public GenreDto getGenreById(Long id) {
-        Genre genre = genreRepository.findById(id).orElseThrow();
+        log.info("Try to find genre by id {}", id);
+        Genre genre = genreRepository.findById(id).orElseThrow(() -> {
+                log.error("Genre with id {} not found", id);
+        return new NoSuchElementException("Genre not found");
+    });
         GenreDto genreDto = convertEntityToDto(genre);
+        log.info("Found genre: {}", genreDto);
         return genreDto;
     }
 
